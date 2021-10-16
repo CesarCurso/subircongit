08fc4970db1d70a532aba660753b9728de4d2c0b
Cesar <Cesar@gmail.com>
2021-10-16 06:19:35 -0300

    subir todo el integrador

---
+++ javascript/ventaticket.js
@@ -0,0 +1,28 @@
+function resumenFuncion() {
+    let cantida =  document.getElementById("cantidaticket").value;
+    let cate = document.getElementById("categoria");
+    let categorias = cate.options[cate.selectedIndex].text;
+    let valor=0;
+    let numero= parseInt(cantida);
+    
+    if ( isNaN(numero)){
+        alert("EN CANTIDAD INGRESE SOLO NUMEROS ENTEROS");
+    }else{
+        switch (categorias) {
+               case "Estudiante":
+                    valor= parseInt(cantida)*200*0.2;
+                    document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                    break;
+              case "Trainee":
+                   valor = parseInt(cantida)*200*0.5; 
+                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                   break;
+             case "Junior":
+                   valor = parseInt(cantida)*200*0.85; 
+                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                   break;
+             default:
+                   break;
+        }/* fin del switch */
+   }/* fin del else */
+}/* fin funcion */
